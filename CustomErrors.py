444251103289204e0a5384e5e62140d9fa5bf1ff
anaviuppal <anaviuppal@gmail.com>
2023-05-05 18:12:30 -0400

    Moved files to meteoreo directory

--- CustomErrors.py
+++
@@ -1,5 +0,0 @@
-class APIError(Exception):
-    """Raised when the lightpollutionmap.info API returns a non-200 response code."""
-    
-    def __init__(self, response_code):
-        self.response_code = response_code
